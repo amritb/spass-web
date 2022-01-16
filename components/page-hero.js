2a9dccbb7803ac5d650463115d3f41a1b1bd4df7
Amrit Bera <amritbera@gmail.com>
2022-01-16 22:24:49 +0100

    Added favicons from favicon.io

--- components/page-hero.js
+++ components/page-hero.js
@@ -12,40 +12,47 @@ export default function PageHero({ title, subTitle, bgSrc = "images/home-hero.jp
 
   return (
     <>
-    <Head>
-      <title>{title} - Spaß Berlin</title>
-    </Head>
-    <div style={bgStyle}>
-      <div className="hero-mask">
-        <Container className="mb-4">
-          <Row className="align-items-end hero-row">
+      <Head>
+        <title>{title} - Spaß Berlin</title>
 
-            {!home ?
-              <Col className="col-12 mb-auto">
-                <Row className="mt-4 d-flex align-items-center">
-                  <Col className="col-6 font-logo text-white display-6">
-                    <Link href="/"><a className="text-white text-decoration-none">{process.env.shortName}</a></Link>
-                  </Col>
-                  <Col className="col-6 text-end">
-                    <Link href="/join-us"><a className="btn btn-warning text-white rounded-pill px-3">Join Us</a></Link>
-                  </Col>
-                </Row>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
 
-              </Col>
-              : null}
 
-            <Col className={`text-center text-white col-12 mb-5 py-4`}>
-              <h1 className={`display-3 ${home ? 'font-logo' : 'gradient-text'}`}>{title}</h1>
+      </Head>
+      <div style={bgStyle}>
+        <div className="hero-mask">
+          <Container className="mb-4">
+            <Row className="align-items-end hero-row">
 
-              {subTitle ?
-                <h2>{subTitle}</h2>
+              {!home ?
+                <Col className="col-12 mb-auto">
+                  <Row className="mt-4 d-flex align-items-center">
+                    <Col className="col-6 font-logo text-white display-6">
+                      <Link href="/"><a className="text-white text-decoration-none">{process.env.shortName}</a></Link>
+                    </Col>
+                    <Col className="col-6 text-end">
+                      <Link href="/join-us"><a className="btn btn-warning text-white rounded-pill px-3">Join Us</a></Link>
+                    </Col>
+                  </Row>
+
+                </Col>
                 : null}
 
-            </Col>
-          </Row>
-        </Container>
+              <Col className={`text-center text-white col-12 mb-5 py-4`}>
+                <h1 className={`display-3 ${home ? 'font-logo' : 'gradient-text'}`}>{title}</h1>
+
+                {subTitle ?
+                  <h2>{subTitle}</h2>
+                  : null}
+
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
     </>
   );
 }
